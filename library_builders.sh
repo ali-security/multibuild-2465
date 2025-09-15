0b1acdff515fff3e4ba56c7be025547a7f739d9c
Tal Samucha <tal.s@sealsecurity.io>
2025-09-15 19:43:21 +0300

    get_modern_cmake install if none

--- library_builders.sh
+++ library_builders.sh
@@ -202,7 +202,16 @@ function get_modern_cmake {
     if cmake --version &> /dev/null; then
         echo "CMake version: $(cmake --version | head -n1)"
     else
-        error "CMake is not installed"
+        error "CMake is not installed", installing CMake ${CMAKE_VERSION}"
+        curl -LO https://cmake.org/files/v3.11/cmake-${CMAKE_VERSION}.tar.gz
+        tar -xzf cmake-${CMAKE_VERSION}.tar.gz
+        cd cmake-${CMAKE_VERSION}
+        ./bootstrap --prefix=/opt/cmake-${CMAKE_VERSION}
+        make -j$(nproc)
+        make install
+        export PATH=/opt/cmake-${CMAKE_VERSION}/bin:$PATH
+        cd ..   # <-- back to repo root, so multibuild/ exists
+        echo "CMake version: $(cmake --version | head -n1)"
     fi
     echo $cmake
 }
