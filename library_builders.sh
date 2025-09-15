077c5d5b9904c5d4574d52f02abc63918108b0a2
Tal Samucha <tal.s@sealsecurity.io>
2025-09-15 21:28:20 +0300

    LCMS2_VERSION

--- library_builders.sh
+++ library_builders.sh
@@ -74,26 +74,28 @@ function build_simple {
     touch "${name}-stamp"
 }
 
-function build_simple_cmake_policy {
+function build_simple_no_ext {
+    # same as build_simple but without modifying the url, caller should provide final url
     # Example: build_simple libpng $LIBPNG_VERSION \
-    #               https://download.sourceforge.net/libpng tar.gz \
+    #               https://download.sourceforge.net/libpng.tar.gz \
     #               --additional --configure --arguments
     local name=$1
     local version=$2
     local url=$3
-    local ext=${4:-tar.gz}
+    local filename=$4
     local configure_args=${@:5}
     if [ -e "${name}-stamp" ]; then
         return
     fi
     local name_version="${name}-${version}"
-    local archive=${name_version}.${ext}
-    fetch_unpack $url/$archive
+    echo "seal build simple no ext: ${name} ${version} ${url} "
+    fetch_unpack $url $filename
     (cd $name_version \
-        && cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX $configure_args \
+        && ./configure --prefix=$BUILD_PREFIX $configure_args \
         && make -j4 \
         && make install)
     touch "${name}-stamp"
+    echo "seal stamped ${name}-stamp"
 }
 
 function build_github {
@@ -267,7 +269,11 @@ function build_openjpeg {
 
 function build_lcms2 {
     build_tiff
-    build_simple_cmake_policy lcms2 $LCMS2_VERSION https://downloads.sourceforge.net/project/lcms/lcms/$LCMS2_VERSION
+    # build_simple lcms2 $LCMS2_VERSION https://downloads.sourceforge.net/project/lcms/lcms/$LCMS2_VERSION
+    echo LCMS2_VERSION: $LCMS2_VERSION
+    # old: https://downloads.sourceforge.net/project/lcms/lcms/2.15/lcms2-2.15.tar.gz
+    build_simple_no_ext lcms2 $LCMS2_VERSION https://sourceforge.net/projects/lcms/files/lcms/2.15/lcms2-2.15.tar.gz/download lcms2-2.15.tar.gz
+    echo "seal done lcms"
 }
 
 function build_giflib {
